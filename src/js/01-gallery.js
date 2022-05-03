ce2f2f0cc0761ed718d85ccb203a0a4dead3fc7e
Maksym Padalka <sheihmaks@gmail.com>
2022-05-04 01:28:34 +0300

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,22 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+
+const galleryItemsCard = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+  })
+  .join('');
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryItemsCard);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
