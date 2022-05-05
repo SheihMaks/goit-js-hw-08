cbe9ad7cf8aa3cf6371afe1be72e87d432b0cb2e
Maksym Padalka <sheihmaks@gmail.com>
2022-05-05 23:41:58 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,25 @@
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
+player.on('timeupdate', function (data) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+});
+const savedTime = localStorage.getItem('videoplayer-current-time');
+const parsedCurrentTime = JSON.parse(savedTime);
+
+player.setCurrentTime(parsedCurrentTime.seconds).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      // the time was less than 0 or greater than the video’s duration
+      break;
+
+    default:
+      // some other error occurred
+      break;
+  }
 });
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
+// localStorage.clear();
