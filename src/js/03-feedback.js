777077b367bb9ac9255fedf72d497ba252e751fc
Maksym Padalka <sheihmaks@gmail.com>
2022-05-08 13:35:50 +0300

    +

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,34 @@
+import throttle from 'lodash.throttle';
 const formRef = document.querySelector('.feedback-form');
-const formInputRef = document.querySelector('input[name = "email"]');
-const formMessageRef = document.querySelector('textarea[name="message"]');
+// const formInputRef = document.querySelector('input[name = "email"]');
+// const formMessageRef = document.querySelector('textarea[name="message"]');
+const FEEDBACK_FORM_STATE = 'feedback-form-state';
+let formData = {};
+const saveData = JSON.parse(localStorage.getItem(FEEDBACK_FORM_STATE));
 
 rewriteValue();
 
-const formData = {};
-
 const onInputValue = ev => {
   formData[ev.target.name] = ev.target.value;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(FEEDBACK_FORM_STATE, JSON.stringify(formData));
 };
 
 const onSubmitForm = ev => {
   ev.preventDefault();
   ev.currentTarget.reset();
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(FEEDBACK_FORM_STATE);
+  console.log(saveData);
 };
 
-formRef.addEventListener('submit', onSubmitForm);
-formRef.addEventListener('input', onInputValue);
-
 function rewriteValue() {
-  const saveData = JSON.parse(localStorage.getItem('feedback-form-state'));
   if (saveData) {
-    Object.keys(saveData).forEach(el => (formData[el].value = saveData[el]));
+    Object.keys(saveData).forEach(key => {
+      formRef[key].value = saveData[key];
+    });
+    // if (saveData.email) formInputRef.value = saveData.email;
+    // if (saveData.message) formMessageRef.value = saveData.message;
   }
-  return;
 }
+
+formRef.addEventListener('input', throttle(onInputValue, 500));
+formRef.addEventListener('submit', onSubmitForm);
